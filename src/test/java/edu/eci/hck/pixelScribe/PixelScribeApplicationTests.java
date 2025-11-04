bea4162e1c69745b1b99a21780c44235a58b1d38
William Hernandez <willicamiher22@gmail.com>
2025-11-04 14:56:21 -0500

    Scaffolding

---
+++ src/test/java/edu/eci/hck/pixelScribe/PixelScribeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.hck.pixelScribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PixelScribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
