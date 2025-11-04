0237c40d6acee598ed87627ead2540c0d6e8c4c2
juanfe-rangel <rangeljuanfe687@gmail.com>
2025-11-04 16:02:35 -0500

    image DTO y image mod

--- src/main/java/edu/eci/hck/pixelScribe/Images/model/Image.java
+++ src/main/java/edu/eci/hck/pixelScribe/Images/model/Image.java
@@ -1,4 +1,20 @@
 package edu.eci.hck.pixelScribe.Images.model;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document(collection = "imagenes")
 public class Image {
+
+    @Id
+    private String id;
+
+    private String nombre;
+    private byte[] datos;
+    private String descripcion;
 }
+
---
+++ src/main/java/edu/eci/hck/pixelScribe/Images/model/imageDTO.java
@@ -0,0 +1,5 @@
+package edu.eci.hck.pixelScribe.Images.model;
+
+public class imageDTO {
+
+}
