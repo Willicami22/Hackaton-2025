4eb99726e3c8c77e50eef1dbd1afd394eeaec7a8
William Hernandez <willicamiher22@gmail.com>
2025-11-04 17:14:38 -0500

    Remove OpenAI API key from project

--- src/main/java/edu/eci/hck/pixelScribe/Images/model/Image.java
+++ src/main/java/edu/eci/hck/pixelScribe/Images/model/Image.java
@@ -3,6 +3,7 @@ package edu.eci.hck.pixelScribe.Images.model;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
@@ -13,6 +14,8 @@ public class Image {
     @Id
     private String id;
     private String nombre;
+
+    @Transient
     private byte[] datos;
     private Description descripcion;
 }
